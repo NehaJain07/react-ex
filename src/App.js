4b758863e44415f6ec58e5a2076e0685e1a55588
Neha Jain <jainn.ne@gmail.com>
2021-07-28 15:57:17 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Blog from './component/Blog'
+import Details from './component/Details'
+import Edit from './component/Edit'
+import Add from './component/Add'
+import './component/style.css'
+import { BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"><Router>
+      <h1 style={{textAlign:"center"} }>Welcome Fetch API</h1>
+      <div style={{textAlign:"center"}}>
+        <Link to="/add" style={{fontSize:"15px"}}>Create Blog</Link>
+      </div>
+      <Switch>
+      <Route path="/" exact component={ Blog } />
+      <Route path="/details/:id" exact component={ Details } />
+      <Route path="/edit/:id" exact component={ Edit } />
+      <Route path="/add" component={ Add } />
+
+      </Switch>
+      </Router>
+      
     </div>
   );
 }
